4354d09d51e25f65915581348d5ce34b0ca02f40
Spring MC <Heresy.MC@gmail.com>
2018-01-06 17:17:07 +0800

    What: always return new logger
    Why:
    
      * cause race issue by share
    
    How:
    
      * nop

--- logger_test.go
+++ logger_test.go
@@ -1,6 +1,7 @@
 package gogo
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/golib/assert"
@@ -18,4 +19,16 @@ func Test_NewAppLogger(t *testing.T) {
 	assertion.Empty(logger.RequestID())
 	assertion.Equal("di-tseuqer-x", newLogger.RequestID())
 	assertion.NotEqual(logger, newLogger)
+}
+
+func Test_Logger_Reuse(t *testing.T) {
+	assertion := assert.New(t)
+
+	logger := NewAppLogger("null", "")
+
+	nlog := logger.New("origin")
+	logger.Reuse(nlog)
+
+	tlog := logger.New("reuse")
+	assertion.Equal(fmt.Sprintf("%p", nlog), fmt.Sprintf("%p", tlog))
 }
